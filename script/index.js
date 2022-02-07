2b8184cf594dcee9077eb4047276b7be377ad03d
maxlmysim <maxlmysim@gmail.com>
2022-02-07 09:21:42 +0300

    refactor: apply prettier

--- script/index.js
+++ script/index.js
@@ -1,33 +1,30 @@
 async function getQuote() {
-    const url = 'https://favqs.com/api/qotd';
-    const res = await fetch(url);
-    const data = await res.json();
+  const url = "https://favqs.com/api/qotd";
+  const res = await fetch(url);
+  const data = await res.json();
 
-    quote.innerHTML = data.quote.body;
-    quoteAuthor.innerHTML = '- ' + data.quote.author;
-    getImage(data.quote.author);
+  quote.innerHTML = data.quote.body;
+  quoteAuthor.innerHTML = "- " + data.quote.author;
+  getImage(data.quote.author);
 }
 
 async function getImage(name) {
-    let nameAuthor = name.split(' ').join('_');
-    let url = `https://en.wikipedia.org/w/api.php?action=query&titles=${nameAuthor}&prop=pageimages&format=json&pithumbsize=250&origin=*`;
-    const res = await fetch(url);
-    const data = await res.json();
-    const numPages = Object.keys(data.query.pages);
-    const linkPage = data.query.pages[numPages[0]].thumbnail?.source
+  let nameAuthor = name.split(" ").join("_");
+  let url = `https://en.wikipedia.org/w/api.php?action=query&titles=${nameAuthor}&prop=pageimages&format=json&pithumbsize=250&origin=*`;
+  const res = await fetch(url);
+  const data = await res.json();
+  const numPages = Object.keys(data.query.pages);
+  const linkPage = data.query.pages[numPages[0]].thumbnail?.source;
 
-    if(linkPage) {
-        document.querySelector('.page-author').src = linkPage
-    } else {
-        document.querySelector('.page-author').src = './assets/img/img.jpeg'
-    }
+  if (linkPage) {
+    document.querySelector(".page-author").src = linkPage;
+  } else {
+    document.querySelector(".page-author").src = "./assets/img/img.jpeg";
+  }
 }
 
-const buttonNewQuote = document.querySelector('.container__button');
-const quote = document.querySelector('.container__quote');
-const quoteAuthor = document.querySelector('.container__quote-author');
+const buttonNewQuote = document.querySelector(".container__button");
+const quote = document.querySelector(".container__quote");
+const quoteAuthor = document.querySelector(".container__quote-author");
 
-
-buttonNewQuote.addEventListener('click', getQuote);
-
-
+buttonNewQuote.addEventListener("click", getQuote);
